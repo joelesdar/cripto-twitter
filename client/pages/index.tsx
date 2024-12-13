91e6578d8b69a30ed48410c6eb8fb2652eeb5465
Joel Esteban Diaz Arévalo <joel-e11@hotmail.com>
2024-12-12 22:00:41 -0500

    Tailwindcss configurado

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,5 +1,10 @@
 import Head from 'next/head'
 
+const style = {
+  wrapper: `flex justify-center h-screen w-screen select-none bg-[#15202b] text-white`,
+  content: `max-w-[1400px] w-2/3 flex justify-between`, // Corregí el nombre
+};
+
 export default function Home() {
   return (
     <>
@@ -9,7 +14,13 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
+      <div className={style.wrapper}>
+        <div className={style.content}>
+          <h2>Sidebar</h2>
+          <h2>Feed</h2>
+          <h2>Widgets</h2>
+        </div>
+      </div>
     </>
   )
 }
