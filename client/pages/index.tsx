21078edea68f086989283e6f256e05385d18d1a7
Joel Esteban Diaz Arévalo <joel-e11@hotmail.com>
2024-12-13 22:08:43 -0500

    Sidebar agregada

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,9 +1,10 @@
 import Head from 'next/head'
+import Sidebar from '../components/Sidebar'
 
 const style = {
   wrapper: `flex justify-center h-screen w-screen select-none bg-[#15202b] text-white`,
-  content: `max-w-[1400px] w-2/3 flex justify-between`, // Corregí el nombre
-};
+  content: `max-w-[1400px] w-2/3 flex justify-between`, 
+}
 
 export default function Home() {
   return (
@@ -16,7 +17,7 @@ export default function Home() {
       </Head>
       <div className={style.wrapper}>
         <div className={style.content}>
-          <h2>Sidebar</h2>
+          <Sidebar/>
           <h2>Feed</h2>
           <h2>Widgets</h2>
         </div>
