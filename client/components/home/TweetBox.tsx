6e4f9fa1fe33636789d3ee1a7dc894391e970d7b
Joel Esteban Diaz Arévalo <joel-e11@hotmail.com>
2024-12-16 16:54:19 -0500

    Terminado: TweetBox

--- client/components/home/TweetBox.tsx
+++ client/components/home/TweetBox.tsx
@@ -1,4 +1,8 @@
-// import 
+import { useState } from "react"
+import { BsCardImage, BsEmojiSmile } from "react-icons/bs"
+import { RiFileGifLine, RiBarChartHorizontalFill } from "react-icons/ri"
+import { IoMdCalendar } from "react-icons/io"
+import { MdOutlineLocationOn } from "react-icons/md"
 
 const style = {
   wrapper: `px-4 flex flex-row border-b border-[#38444d] pb-4`,
@@ -15,6 +19,13 @@ const style = {
 }
 
 const TweetBox = () => {
+
+  const [tweetMessage, setTweetMessage] = useState('')
+  const postTweet = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault()
+    console.log(tweetMessage)
+  }
+
   return (
     <div className={style.wrapper}>
       <div className={style.tweetBoxLeft}>
@@ -24,7 +35,37 @@ const TweetBox = () => {
           className={style.profileImage}
         />
       </div>
-      <div className={style.tweetBoxRight}></div>
+      <div className={style.tweetBoxRight}>
+        <form>
+          <textarea
+            className={style.inputField}
+            placeholder="What's happening?"
+            value={tweetMessage}
+            onChange={(e) => setTweetMessage(e.target.value)}
+          />
+          <div className={style.formLowerContainer}>
+            <div className={style.iconsContainer}>
+              <BsCardImage className={style.icon} />
+              <RiFileGifLine className={style.icon} />
+              <RiBarChartHorizontalFill className={style.icon} />
+              <BsEmojiSmile className={style.icon} />
+              <IoMdCalendar className={style.icon} />
+              <MdOutlineLocationOn className={style.icon} />
+            </div>
+            <button
+              type="submit"
+              disabled={!tweetMessage}
+              onClick={(event) => postTweet(event)}
+              className={`${style.submitGeneral} ${
+                tweetMessage ? style.activeSubmit : style.inactiveSubmit  
+              }`}
+              
+            >
+              Tweet
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
